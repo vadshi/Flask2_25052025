f0dcb6707ee48d564fbc6e5cf2d3c2d346e9d152
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-05-25 14:22:36 +0300

    Add api initialization

---
+++ QuoteApi/api/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from flask_migrate import Migrate
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+app = Flask(__name__)
+app.json.ensure_ascii = False
+
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+migrate = Migrate(app, db)
+
+# TODO. Обязательно добавить импорт для обработчиков author и quote
