1237bf9b82f609a1f0aecf948361c03196d47caf
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-06-08 12:56:33 +0300

    Add user handlers

--- QuoteApi/api/handlers/user.py
+++ QuoteApi/api/handlers/user.py
@@ -1,17 +1,32 @@
+from flask import abort, jsonify, request
+from marshmallow import ValidationError
 from api import app, db
 from api.models.user import UserModel
+from api.schemas.user import user_schema
+
 
 # url: /users/<int:user_id> - GET
+@app.get("/users/<int:user_id>")
 def get_user_by_id(user_id: int):
-    pass
+    user = db.get_or_404(UserModel, user_id, description=f"User with id={user_id} not found")
+    return jsonify(user_schema.dump(user)), 200
 
 
 # url: /users - GET
+@app.get("/users")
 def get_users():
-    pass
+    users = db.session.scalars(db.select(UserModel)).all()
+    return jsonify(user_schema.dump(users, many=True)), 200
 
 
 
 # url:  /users - POST
+@app.post("/users")
 def create_user():
-    pass
+    try:
+        user = user_schema.loads(request.data)
+        user.save()
+    except ValidationError as ve:
+        abort(400, f"Validation error: {ve.messages_dict}")
+    
+    return jsonify(user_schema.dump(user)), 201
