cb957ee546938da0eaca2cbba97e78cadbd3fac7
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-06-15 11:38:26 +0300

    Add apiflask demo example

---
+++ APIFlask_demo/app.py
@@ -0,0 +1,57 @@
+from apiflask import APIFlask, HTTPError
+from apiflask import Schema
+from apiflask.fields import Integer, String
+from apiflask.validators import Length, OneOf
+
+
+app = APIFlask(__name__, title="Example API", version="1.0")
+app.tags = ["pets"]
+
+class PetIn(Schema):
+    name = String(required=True, validate=Length(3, 10))
+    category = String(required=True, validate=OneOf(['dog', 'cat']))
+
+
+class PetOut(Schema):
+    id = Integer()
+    name = String()
+    category = String()
+
+
+@app.get("/pets/<int:pet_id>")
+@app.output(PetOut)
+@app.doc(summary="Get pet by id", description="Get pet by id", tags=["pets"])
+def get_pet_by_id(pet_id: int):
+    if pet_id == 2:
+        raise HTTPError(404, f"Pet with id={pet_id} not found")
+    return {
+        "id": pet_id,
+        "name": "Coco",
+        "category": "dog"
+    }
+
+@app.get("/pets")
+@app.output(PetOut(many=True))
+@app.doc(summary="Get all pets", description="Get all pets", tags=["pets"])
+def get_pets():
+    return [{
+            "id": 1,
+            "name": "Coco",
+            "category": "dog"
+        },
+        {
+            "id": 2,
+            "name": "Murka",
+            "category": "cat"
+        }
+    ]
+
+
+@app.post("/pets")
+@app.input(PetIn, location="json", arg_name='pet')
+@app.output(PetOut, status_code=201)
+@app.doc(summary="Create new pet", description="Create new pet", tags=["pets"])
+def create_pet(pet):
+    print(pet, type(pet))
+    pet.update({"id": 101})
+    return pet
