85fb36a904bddd6a79d48f41c6503378ed5b9e8f
Vadim Shikhovtsov <vadimx30@gmail.com>
2025-06-15 10:56:13 +0300

    Add flasgger

--- QuoteApi/api/__init__.py
+++ QuoteApi/api/__init__.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import DeclarativeBase
 from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth, MultiAuth
+from flasgger import Swagger
 
 class Base(DeclarativeBase):
     pass
@@ -21,7 +22,7 @@ ma.init_app(app)
 basic_auth = HTTPBasicAuth()
 token_auth = HTTPTokenAuth(scheme="Bearer")
 multi_auth = MultiAuth(basic_auth, token_auth)
-
+swagger = Swagger(app)
 
 @basic_auth.verify_password
 def verify_password(username, password):
--- QuoteApi/api/handlers/user.py
+++ QuoteApi/api/handlers/user.py
@@ -8,6 +8,31 @@ from api.schemas.user import user_schema
 # url: /users/<int:user_id> - GET
 @app.get("/users/<int:user_id>")
 def get_user_by_id(user_id: int):
+    """
+    Get User by id
+    ---
+    tags:
+        - Users
+    parameters:
+         - in: path
+           name: user_id
+           type: integer
+           required: true
+           default: 1
+
+    responses:
+        200:
+            description: A single user item
+            schema:
+                id: User
+                properties:
+                    id:
+                        type: integer
+                    username:
+                        type: string
+                    is_staff:
+                        type: boolean
+    """
     user = db.get_or_404(UserModel, user_id, description=f"User with id={user_id} not found")
     return jsonify(user_schema.dump(user)), 200
 
@@ -15,6 +40,12 @@ def get_user_by_id(user_id: int):
 # url: /users - GET
 @app.get("/users")
 def get_users():
+    """
+    Get all Users
+    ---
+    tags:
+     - Users
+    """
     users = db.session.scalars(db.select(UserModel)).all()
     return jsonify(user_schema.dump(users, many=True)), 200
 
